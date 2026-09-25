85795cdc5d29b1e047b933b922244c076e9d8574
yjtank <valdesonle@gmail.com>
2026-09-25 06:42:22 -0300

    Primeiro commit

---
+++ src/main/java/com/javinha/CadastroDeFilmes/CadastroDeFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.javinha.CadastroDeFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javinha/CadastroDeFilmes/CadastroDeFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javinha.CadastroDeFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
